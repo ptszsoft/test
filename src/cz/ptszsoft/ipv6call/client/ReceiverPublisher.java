b609ddbaac9ce0f53c7123646b1d40f8eb0e8b17
jzeman <jzeman@10.1.0.8>
2017-03-26 20:12:41 +0200

    Druhe nalití

--- src/cz/ptszsoft/ipv6call/client/ReceiverPublisher.java
+++ src/cz/ptszsoft/ipv6call/client/ReceiverPublisher.java
@@ -3,13 +3,14 @@ package cz.ptszsoft.ipv6call.client;
 import javax.swing.SwingUtilities;
 import javax.xml.ws.Endpoint;
 
+import cz.genisoft.library.ip.IPv6info;
 import cz.ptszsoft.ipv6call.client.webservice.WebServiceImplementation;
 
 public class ReceiverPublisher {
 
 	public static void main(String[] args) {
 		try {
-			String wsURL = "https://[" + IPv6info.getLocalIPv6() + "]:9997/ipv6client";
+			String wsURL = "http://[" + IPv6info.getLocalIPv6() + "]:9997/ipv6client";
 			System.out.println(wsURL + "?wsdl");
 			Endpoint.publish(wsURL, new WebServiceImplementation());
 			SwingUtilities.invokeLater(new Runnable() {
